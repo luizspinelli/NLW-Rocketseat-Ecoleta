06d8b0bb6cfd9ed2187cbe52e9b1654633e11c11
Luiz Spinelli <luiz_lucchi@hotmail.com>
2020-06-07 13:57:38 -0300

    validação backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemsController';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -19,6 +20,23 @@ routes.get('/point', pointsController.index);
 
 routes.get('/point/:id', pointsController.show);
 
-routes.post('/point', upload.single('image') ,pointsController.create);
+routes.post('/point',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.string().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    },{
+        abortEarly: false
+    }),
+    pointsController.create
+);
 
 export default routes;
